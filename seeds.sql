d34eae3a950631f295383d502ef127340ebcd31e
Christine Ross <christinejoyross@gmail.com>
2020-09-28 14:15:17 -0700

    seeds

---
+++ seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) 
+VALUES ("Big Mac");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Double Whopper");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Famous Star");
