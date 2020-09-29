33c69ae0b2c0ad98a65b709c6a8c125f9d1ae2ca
Christine Ross <christinejoyross@gmail.com>
2020-09-29 12:46:20 -0700

    orm insertOne

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,24 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
+
+var orm = {
+
+    selectAll: function(tableInput){
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], function(err, result){
+            if (err) throw err;
+
+            console.log(results)
+        })
+    },
+
+    insertOne: function(){
+        var queryString = "INSERT INTO ?? (?) VALUES (?)"
+        connection.query(queryString, [table, col, vals], function(err, result){
+            if (err) throw err;
+
+            console.log(result)
+        })
+    }
+
+}
