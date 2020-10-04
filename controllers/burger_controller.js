4b7bb904bb26da18962d8475087eb6ff77f5c588
Christine Ross <christinejoyross@gmail.com>
2020-10-03 18:08:07 -0700

    jawsdb

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -5,47 +5,49 @@ var burger = require("../models/burger.js");
 var router = express.Router();
 
 
-router.get("/", function(req, res){
-    burger.selectAll(function(data){
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
 
         // console.log(hbsObject);
-        
+
 
         res.render("index", hbsObject);
     })
 })
 
-router.post("/api/burgers", function(req,res){
-    burger.insertOne(
-        ["burger_name"], 
-        [req.body.burger_name], 
-        function(result){
-        res.json({id: result.insertId})
-    })
-})
-
-router.put("/api/burgers/:id", function (req, res){
+router.put("/api/burgers/:id", function (req, res) {
 
     var condition = "id = " + req.params.id;
     console.log("condition: " + condition);
 
     burger.updateOne(
         {devoured: req.body.devoured},
-    condition, function(result){
-        // if (result.changedrows == 0){
-        //     return res.status(404).end();
-        // } else {
-            console.log("successfully devoured")
-        //     res.status(200).end();
-        // }
+        condition, function (result) {
+            if (result.changedRows === 0) {
 
-        
-    }
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+
+
+        }
     )
 })
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(
+        ["burger_name"],
+        [req.body.burger_name],
+        function (result) {
+            res.json({ id: result.insertId })
+        })
+})
+
+
 
 
 
--- public/assets/js/burgerscript.js
+++ public/assets/js/burgerscript.js
@@ -1,23 +1,5 @@
 $(function () {
-    $(".eat-burger").on("click", function (event) {
-        event.preventDefault();
-        console.log("working")
-        var id = $(this).data("id");
-
-        var newEatState = {
-            devoured: 1
-        };
-
-        $.ajax("/api/burgers/" +id, {
-            type: "PUT",
-            data: newEatState
-        }).then(function(){
-            console.log("changed status to ", newEatState)
-            location.reload();
-        })
-
-    });
-
+   
     $(".create-form").on("submit", function(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
@@ -38,4 +20,24 @@ $(function () {
           }
         );
       });
+
+      $(".eat-burger").on("click", function (event) {
+        event.preventDefault();
+        console.log("working")
+        var id = $(this).data("id");
+
+        var newEatState = {
+            devoured: 1
+        };
+
+        $.ajax("/api/burgers/" +id, {
+            type: "PUT",
+            data: newEatState
+        }).then(function(){
+            console.log("changed status to ", newEatState)
+            location.reload();
+        })
+
+    });
+
 })
